65e74dbb375afbb0a3c2dfd084da55e7a8e05436
hotblack43 <peter.thejll@gmail.com>
2019-12-14 13:19:06 +0100

    is.even and is.odd insteadx of even and odd - requires library(schoolmath)

--- R/phase_scramble2.R
+++ R/phase_scramble2.R
@@ -2,16 +2,17 @@ phase_scramble2 <- function(series){
 # Perform 'phase-scrambling' on an input series given the series
 # Uses code from BOC
 # NOTE: output is returned in the input argument
+# NB: this code needs library(schoolmath)
   fft_signal <- fft(series)
   sd_org <- sd(series)
 
   nmax <- length(fft_signal)
-  if (even(nmax)) {   # nmax  even
+  if (is.even(nmax)) {   # nmax  is.even
     theta <- runif(nmax/2-1,min=0,max=2.*pi)
     factor <- complex(real=cos(theta),imaginary=sin(theta))
     factor <- c(1,factor,1,Conj(rev(factor[])))
   }
-  if (odd(nmax)) {
+  if (is.odd(nmax)) {
     theta <- runif(nmax/2,min=0,max=2.*pi)
     factor <- complex(real=cos(theta),imaginary=sin(theta))
     factor <- c(1,factor,Conj(rev(factor[])))
